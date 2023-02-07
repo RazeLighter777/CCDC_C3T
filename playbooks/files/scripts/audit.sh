4fb87017e939122fa7147b7c9cc7b77a3a6e271f
Justin Suess <justin.suess@westpoint.edu>
2023-02-07 13:58:29 -0500

    refactoring

--- playbooks/files/scripts/audit.sh
+++ playbooks/files/scripts/audit.sh
@@ -35,4 +35,6 @@ echo '----------------'
 find /home -name '*.mp3' -o -name '*.mp4' -o -name '*.avi' -o -name '*.mov' -o -name '*.flv' -o -name '*.wmv' -o -name '*.mkv' -o -name '*.mpg' -o -name '*.mpeg' -o -name '*.m4v' -o -name '*.ogg' -o -name '*.wav' -o -name '*.flac' -o -name '*.wma' -o -name '*.m4a' -o -name '*.aac' -o -name '*.jpg' -o -name '*.jpeg' -o -name '*.png' -o -name '*.gif' -o -name '*.bmp' -o -name '*.tiff' -o -name '*.tif' -o -name '*.svg' -o -name '*.psd' -o -name '*.ai' -o -name '*.eps' -o -name '*.indd' -o -name '*.raw' -o -name '*.cr2' -o -name '*.nef' -o -name '*.orf' -o -name '*.sr2' -o -name '*.srf' -o -name '*.arw' -o -name '*.dng' -o -name '*.webp' -o -name '*.3fr' -o -name '*.cine' -o -name '*.crw' -o -name '*.dcr' -o -name '*.k25' -o -name '*.kdc' -o -name '*.mdc' -o -name '*.mef' -o -name '*.mos' -o -name '*.mrw' -o -name '*.nrw' -o -name '*.pef' -o -name '*.ptx' -o -name '*.pxn' -o -name '*.r3d' -o -name '*.raf' -o -name '*.rw2' -o -name '*.rwl' -o -name '*.rwz' -o -name '*.srw' -o -name '*.x3f' -o -name '*.pdf' -o -name '*.doc' -o -name '*.docx' -o -name '*.xls' -o -name '*.xlsx' -o -name '*.ppt' -o -name '*.pptx' 
 echo 'Locally installed executables:'
 echo '----------------'
-
+echo 'Enlessh logs (/var/log/enlessh.log):'
+echo '----------------'
+cat /var/log/endlessh.log || true
