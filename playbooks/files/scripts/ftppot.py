fd3120242c09d87c8f6b4870f2f4702182ebdb26
kdeary <korbin.deary45@gmail.com>
2023-02-08 17:22:06 -0500

    Added ftppot python script

---
+++ playbooks/files/scripts/ftppot.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+######## Imports ########
+import os, socket
+
+try:
+  from pyftpdlib.servers import FTPServer
+except:
+  os.system("pip3 install pyftpdlib==1.5.7")
+finally:
+  from pyftpdlib.handlers import FTPHandler
+  from pyftpdlib.servers import FTPServer
+  from pyftpdlib.authorizers import DummyAuthorizer
+  from pyftpdlib.filesystems import UnixFilesystem
+
+
+######## Global Variables ########
+USR = "ftpuser"
+PWD = "ftp"
+PORT = 4444
+linPath = "/home/ftp"
+
+######## Helper Function ########
+def createFTPDirectory() -> None:
+  if not os.path.exists(linPath):
+    os.mkdir(linPath)
+
+def runFTPServer() -> None:
+  createFTPDirectory()
+  authorizer = DummyAuthorizer()
+  authorizer.add_user("c3t", "c3t",".", perm="elradfmwMT")
+
+  handler = FTPHandler
+  handler.authorizer = authorizer
+  handler.passive_ports = range(4445, 4450)
+  
+  # Define a customized banner (string returned when client connects)
+  handler.banner = "pyftpdlib based ftpd ready."
+  
+  server = FTPServer(('', PORT), handler)
+
+  server.max_cons = 256
+  server.max_cons_per_ip = 5
+
+  server.serve_forever()
+
+
+if __name__ == '__main__':
+  runFTPServer()
